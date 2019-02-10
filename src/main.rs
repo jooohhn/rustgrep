0a2eaed2e96a58af5019437184ce7505bdad1678
John Tran <jptran318@gmail.com>
2019-02-09 21:42:54 -0800

    Added file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::fs;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("{}", contents);
 }
