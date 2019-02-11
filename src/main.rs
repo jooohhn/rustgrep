a86c149ce3136240d1939f941a31e78457f3fbf0
John Tran <jptran318@gmail.com>
2019-02-10 18:29:17 -0800

    Modularized main function into functions

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,44 @@
 use std::fs;
 use std::env;
+use std::process;
+use std::error::Error;
 
+// Sets up configuration then runs program
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
-    let contents = fs::read_to_string(filename)
-        .expect("Something went wrong reading the file");
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = run(&config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: &Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(&config.filename)?;
 
     println!("{}", contents);
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            Err("Not enough arguments")
+        } else {
+            let query = args[1].clone();
+            let filename = args[2].clone();
+            Ok(Config { query: query, filename: filename})
+        }
+    }
 }
