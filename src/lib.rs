44955c92d8344a8503e9e6928f300741e714b79d
John Tran <jptran318@gmail.com>
2019-02-14 17:20:13 -0800

    Added case-insensitive env option

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 use std::fs;
 use std::error::Error;
+use std::env; 
 
 pub fn run(config: &Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(&config.filename)?;
-    for line in search(&config.query, &contents) {
+
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{}", line);
     }
+
     Ok(())
 }
 
@@ -19,19 +28,36 @@ fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     res
 }
 
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
 pub struct Config {
-    query: String,
-    filename: String,
+    pub query: String,
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            Err("Not enough arguments")
-        } else {
-            let query = args[1].clone();
-            let filename = args[2].clone();
-            Ok(Config { query: query, filename: filename})
+            return Err("not enough arguments");
         }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config { query, filename, case_sensitive })
     }
 }
