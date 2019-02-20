b1b3fb765bdd17269d82325ca21555280c8510da
John Tran <jptran318@gmail.com>
2019-02-19 20:13:43 -0800

    Added tests

--- src/lib.rs
+++ src/lib.rs
@@ -61,3 +61,38 @@ impl Config {
         Ok(Config { query, filename, case_sensitive })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
+}
